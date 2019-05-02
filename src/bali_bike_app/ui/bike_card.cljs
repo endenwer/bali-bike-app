139effcdf2641507d8cec2c442576a3c887d90a8
Stepan Lusnikov <endenwer@gmail.com>
2019-05-02 10:08:01 +0800

    Fix styles for bikes

--- src/bali_bike_app/ui/bike_card.cljs
+++ src/bali_bike_app/ui/bike_card.cljs
@@ -1,9 +1,10 @@
 (ns bali-bike-app.ui.bike-card
-  (:require [bali-bike-app.constants :as constants]))
+  (:require [bali-bike-app.constants :as constants]
+            [reagent.core :as r]))
 
 (defn main
   [{:keys [model-id photos]}]
   [:div.bike-card
-   [:div.photos]
+   [:div.photo {:style {:background-image (str "url(\"" (first photos) "\")")}}]
    [:div.title (get constants/models model-id)]])
 
