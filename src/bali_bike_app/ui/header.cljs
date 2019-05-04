697492b64d1996c06a7e14e4da7dfc58aa7b5d44
Stepan Lusnikov <endenwer@gmail.com>
2019-05-04 15:34:16 +0800

    Fix bike model input filtering

--- src/bali_bike_app/ui/header.cljs
+++ src/bali_bike_app/ui/header.cljs
@@ -3,26 +3,35 @@
             [reagent.core :as r]
             [re-frame.core :as rf]))
 
-(defn main []
+(defn render-search []
   (r/with-let [constants (rf/subscribe [:constants])
                filters (rf/subscribe [:filters])
                on-model-search #(rf/dispatch [:add-filters {:model-id %}])]
-    [:div.header
-     [:a {:href "/"}
-      [:img.logo {:src "img/logo.svg"}]]
-     [ant/select {:show-arrow false
-                  :show-search true
-                  :class-name "model-search"
-                  :placeholder "Search"
-                  :allow-clear true
-                  :on-change on-model-search}
-      (for [[id title] (:models @constants)]
-        ^{:key id} [ant/select-option {:value id} title])]
-     [:div.links
-      [:a {:href "https://dashboard.balibike.app" :target "_blank"} "Add bike"]
-      [:a {:href "https://itunes.apple.com/us/app/balibike/id1456497891?ls=1&mt=8"
-           :target "_blank"}
-       [:img.store-img {:src "img/badgeappstore.png"}]]
-      [:a {:href "https://play.google.com/store/apps/details?id=app.balibike"
-           :target "_blank"}
-       [:img.store-img {:src "img/badgegoogleplay.png"}]]]]))
+    [ant/select {:show-arrow false
+                 :show-search true
+                 :class-name "model-search"
+                 :placeholder "Search"
+                 :allow-clear true
+                 :on-change on-model-search
+                 :option-filter-prop "children"
+                 :filter-option (fn [input option]
+                                  (>= (.indexOf
+                                       (.props.children.toLowerCase option)
+                                       (.toLowerCase input))
+                                      0))}
+     (for [[id title] (sort-by val (:models @constants))]
+       ^{:key id} [ant/select-option {:value id} title])]))
+
+(defn main []
+  [:div.header
+   [:a {:href "/"}
+    [:img.logo {:src "img/logo.svg"}]]
+   [render-search]
+   [:div.links
+    [:a {:href "https://dashboard.balibike.app" :target "_blank"} "Add bike"]
+    [:a {:href "https://itunes.apple.com/us/app/balibike/id1456497891?ls=1&mt=8"
+         :target "_blank"}
+     [:img.store-img {:src "img/badgeappstore.png"}]]
+    [:a {:href "https://play.google.com/store/apps/details?id=app.balibike"
+         :target "_blank"}
+     [:img.store-img {:src "img/badgegoogleplay.png"}]]]])
